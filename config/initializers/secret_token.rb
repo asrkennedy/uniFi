46e12f79a662cda2a10757e5731b7d04c183347d
Luke Robertson <luke.robertson@mac.com>
2013-08-01 16:05:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnifiApp::Application.config.secret_token = '4f446512e40f77d0991271db2ff0d8935cb438a66590d4f35b67be3f029b9930bde1c655abdd7f38321315a5feb7f282a408870cb18409075fb35b4cd85aa32e'
