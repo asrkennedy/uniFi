25e5cac9c48ec8880f73a4c3cc244f5bbf9dc3f4
Luke Robertson <luke.robertson@mac.com>
2013-08-03 10:07:05 +0100

    nested form for user_network now creates user_network and parent wifi-network (and edits).  Still need to add before filter to check whether parent wifi_network exists

--- app/models/user_network.rb
+++ app/models/user_network.rb
@@ -1,12 +1,13 @@
 class UserNetwork < ActiveRecord::Base
-  attr_accessible :nickname, :user_id, :user_score, :user_sharing_pref, :wifi_network_id
+  attr_accessible :nickname, :user_id, :user_score, :user_sharing_pref, :wifi_network_id, :wifi_network_attributes
   belongs_to :wifi_network
   belongs_to :user
 
   validates :user_sharing_pref, inclusion: { in: User::SHARING_PREFERENCES }
-  accepts_nested_attributes_for :wifi_network, allow_destroy: false
+  accepts_nested_attributes_for :wifi_network
 
-  before_save :validates_uniqueness_of_children_network
+  # before_save :validates_uniqueness_of_children_network
+
   def shareable_with(user)
     owner_friendship_as_proposer = Friendship.where({proposer_id: self.user_id, proposee_id: user.id}).first
     owner_friendship_as_proposee = Friendship.where({proposee_id: self.user_id, proposer_id: user.id}).first
