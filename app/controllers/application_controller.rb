623a6df28a1aa60197b67e4c18b71a1522a89d45
Andrea Kennedy <asrkennedy@gmail.com>
2013-08-04 14:17:35 +0100

    implemented carrierwave and cancan, cancan not complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-
+rescue_from CanCan::AccessDenied do |exception|
+ redirect_to root_url , alert: "You can't access this page"
+end
 
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,8 @@ UnifiApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  CarrierWave.configure do |config|
+  config.storage = :file
+end
 end
