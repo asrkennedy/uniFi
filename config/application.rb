0580484107d00c6bda8757fab95bda7422ec0a07
Luke Robertson <luke.robertson@mac.com>
2013-08-08 13:54:46 +0100

    fake commit

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module UnifiApp
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
