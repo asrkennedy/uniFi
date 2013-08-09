8ec302787da1156c0fc173b757767b341cc1f5bc
Luke Robertson <luke.robertson@mac.com>
2013-08-09 08:55:41 +0100

    stops reciprocal friend requests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,9 @@ class Friendship < ActiveRecord::Base
 
   validates_uniqueness_of :proposer_id, scope: [:proposee_id]
 
+
+
+
 # def show_users
 #   User.all do |user|
 #     user.first_name + user.last_name
@@ -13,5 +16,13 @@ class Friendship < ActiveRecord::Base
 #     user.friends.size
 #     end
 # end
+
+  def reciprocal_friendship_doesnt_exist
+    reciprocal_friendships = Friendship.where({proposer_id: self.proposee_id, proposee_id: self.proposer_id})
+    if reciprocal_friendships.any?
+      errors.add(:base, "You already have a friend request from #{proposee.full_name}")
+    end
+  end
+
 
 end
